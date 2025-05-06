6e5e6df3b78c4b75f1adf1090d3956bb5d29233a
Sulthonhanan <hananakhnaf31@gmail.com>
2025-05-06 11:12:19 +0700

    Menambahkan contoh lain dari pointer

---
+++ pointer2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+{
+    public:
+    int nim;
+    void shownim()
+    {
+        cout << "No induk = " << nim << endl;
+    }
+};
+
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1};  //pointer objek mhs
+    mhs->nim = 2;
+    mhs->shownim();
+   
+    delete mhs;
+    return 0;
+}
