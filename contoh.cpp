e1f142823e710b0ccc6d36e91dda49d023c8b00c
Sulthonhanan <hananakhnaf31@gmail.com>
2025-05-06 10:31:06 +0700

    Penambahan Class

---
+++ contoh.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Persegi_Panjang
+{
+    public:
+        int panjang, lebar;
+
+
+        public:
+
+        void input()
+        {
+            cout << "Masukan Panjangnya :";
+            cin >> panjang;
+            cout << "Masukan Lebarnya :";
+            cin >> lebar;
+        }
+};
